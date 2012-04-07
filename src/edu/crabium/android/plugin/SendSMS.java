48cbeea45ae218ef33f48e36590dbaa5b3ea62cc
tikiet <tikiet@gmail.com>
2012-04-07 13:31:50 +0800

    refactor SendSMS: add comments

--- src/edu/crabium/android/plugin/SendSMS.java
+++ src/edu/crabium/android/plugin/SendSMS.java
@@ -1,7 +1,5 @@
 package edu.crabium.android.plugin;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,55 +18,77 @@ import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.telephony.SmsManager;
-import android.util.Log;
 
+/**
+ * Plug-in class that is called after a phone call was missed.
+ * Send message to caller, with previously chosen text
+ */
 public class SendSMS implements Runnable{
 	private SettingProvider sp = SettingProvider.getInstance();
     Context context = sp.getContext();
 	ContentResolver contentResolver = context.getContentResolver();
 
+	public void run() {
+		send();
+	}
+
 	private void send() {
 		if(!iMissIsOn()) return;
-
-		String ringingNumber;
-		ringingNumber  = IMissPhoneStateListener.RingingNumber;
+		
+		// Retrieve caller number from IMissPhoneStateListener
+		String ringingNumber  = IMissPhoneStateListener.RingingNumber;
+		if(ringingNumber == null || ringingNumber.trim().equals("")) return;
+		
+		// Get caller name from contacts by phone number.
+		// Set name to blank if number isn't in contacts.
 		String callerName = getNameByNumber(ringingNumber);
+		
 		StringBuilder smsTextBuilder = new StringBuilder();
 		StringBuilder notifySummaryBuilder = new StringBuilder();
 		notifySummaryBuilder.append("发送了一条短信");
 		
+		// If caller name is not blank ( caller is in contacts )
 		if(!callerName.trim().equals("")){
+			// Find group id by caller's number, and use group id to retrieve group message
 			int groupId = sp.getGroupIdByPhoneNumber(ringingNumber);
 			String message =  sp.getMessageByGroupId(groupId);
 			
+			// If group message is not blank, use this message as reply text,
+			// or use default reply text in "contacts_reply" instead.
+			//TODO: change contacts_reply to a more vivid name
 			smsTextBuilder.append(message.trim().equals("")? sp.getSetting("contacts_reply") : message);
 			notifySummaryBuilder.insert(0,"向" + callerName);
 		}
 		else{
+			// If user choose to reply to stranger, use default reply text in "stranger_reply", or
+			// simply return from send()
+			if(!replyToStranger()) return;
 			smsTextBuilder.append(replyToStranger() ? sp.getSetting("stranger_reply") : " ");
 			notifySummaryBuilder.insert(0,"向" + ringingNumber);
 		}
 		
+		// Add [iMiss] to the end of SMS message, inform receiver that this message is sent by application.
 		smsTextBuilder.append("[iMiss]");
 		
-		Log.d("iMiss V1.0", "Sending SMS, number:" +ringingNumber + ", text:" + smsTextBuilder.toString()); 
-		
-		if(ringingNumber != null && !ringingNumber.trim().equals("")){
-			String smsText = smsTextBuilder.toString();
-			SmsManager sm = SmsManager.getDefault();
-			sm.sendTextMessage(ringingNumber, null, smsText, null, null);
+		// Send message to caller
+		String smsText = smsTextBuilder.toString();
+		SmsManager sm = SmsManager.getDefault();
+		sm.sendTextMessage(ringingNumber, null, smsText, null, null);
 
+		// If notification is turned on, notify user in status bar
+		if(informOwner()){
 			String notifyBodyHeader = "发送内容：";
 			String notifyBody = notifyBodyHeader + smsText;
 			String notifySummary = notifySummaryBuilder.toString();
 			nofity(notifySummary, notifySummary, notifyBody);
 		}
-	}
-
-	public void run() {
-		send();
+		
 	}
 	
+	private boolean informOwner(){
+		String InformSwitch = "inform_switch";
+		return sp.getSetting(InformSwitch).equals("true") ? true : false;
+	}
 	private boolean replyToStranger(){
 		String StrangerSwitch = "stranger_switch";
 		return sp.getSetting(StrangerSwitch).equals("true") ? true : false;
@@ -89,6 +109,8 @@ public class SendSMS implements Runnable{
         Intent notificationIntent = new Intent(context, IMissActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         notification.setLatestEventInfo(context, notifyTitle, notifyBody, contentIntent);
+        
+        // Clear notification in status bar after user selection
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
         
         notificationManager.notify(1, notification);
@@ -114,13 +136,19 @@ public class SendSMS implements Runnable{
 			            while(phone.moveToNext()) {
 			                String phoneNumber = phone.getString(phone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 			    			Matcher matcher = pattern.matcher(phoneNumber);
+			    			
+			    			// Remove all hyphens in ringing number
 			    			String num = matcher.replaceAll("");
+			    			
+			    			// If an entry's number is matched with ringing number, return that entry's name column immediately
 			    			if(num.equals(RingingNumber))
 			    				return name;
 			            }
 		        }
         }
         finally{
+        	// Close all the cursors.
+        	// Exception may occur if the cursor hasn't been set when closing.
         	if (phone != null)
         		try{
         			phone.close();
