49f6e6d30dfe3dc8aa92b1d6216a29ef3f5972b7
tikiet <tikiet@gmail.com>
2012-04-08 10:01:26 +0800

    rename packages

--- src/edu/crabium/android/GlobalVariable.java
+++
@@ -1,9 +0,0 @@
-package edu.crabium.android;
-
-public class GlobalVariable {
-	//设置拒接时间段时全局变量，待删除
-	public static int TargetStartTimeHour = 00;
-	public static int TargetStartTimeMins = 00;
-	public static int TargetEndTimeHour = 00;
-	public static int TargetEndTimeMins = 00;
-}
---
+++ src/edu/crabium/android/imiss/GlobalVariable.java
@@ -0,0 +1,9 @@
+package edu.crabium.android.imiss;
+
+public class GlobalVariable {
+	//设置拒接时间段时全局变量，待删除
+	public static int TargetStartTimeHour = 00;
+	public static int TargetStartTimeMins = 00;
+	public static int TargetEndTimeHour = 00;
+	public static int TargetEndTimeMins = 00;
+}
